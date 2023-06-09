18defdc930f1cf06793b4a413a403b5ad537ff91
Glydewell Burdick <glydewellb@gmail.com>
2023-06-09 14:11:15 -0500

    add constant feed response

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const crypto = require('crypto');
 
 const server = express();
 const PORT = process.env.PORT || 3000;
+const tempFeed =     [
+    {
+        "uid": crypto.randomUUID(),
+        "updateDate": new Date().toISOString(),
+        "titleText": "Kraft test update",
+        "mainText": "Larry Kraft is your state representative, elected to represent state district 46A in the 2022 election that established the DFL trifecta currently in power in Minnesota.",
+        "streamUrl": "",
+        "redirectionUrl": "https://www.house.mn.gov/members/profile/15619"
+    }
+];
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.type('application/json').json(tempFeed)
+});
+
 server.listen(PORT, () => {
     console.log("The server is listening.")
 });
